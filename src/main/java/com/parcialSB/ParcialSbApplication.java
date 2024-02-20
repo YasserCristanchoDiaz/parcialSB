976f1240bc7aa49aa8b169f2afdb4d3635cb99f0
YasserCristanchoDiaz <yasser.cristancho@uptc.edu.co>
2024-02-20 17:29:40 -0500

    Initial commit

---
+++ src/main/java/com/parcialSB/ParcialSbApplication.java
@@ -0,0 +1,13 @@
+package com.parcialSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parcialSB/ParcialSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcialSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
